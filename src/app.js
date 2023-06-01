d042432807b94019850329764eb36b195e62983e
levy <lac4@aluno.ifal.edu.br>
2023-06-01 11:13:51 -0300

    busca de nome de aluno

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 
 import database from "./db.js";
 import Aluno from "./aluno.js";
+import { Sequelize } from "sequelize";
 
 const app = express();
 
@@ -21,6 +22,10 @@ app.get('/alunos', async (req, res) => {
     res.render('alunos.ejs', {alunos: await Aluno.findAll()});
 });
 
+app.post('/alunos', async (req, res) => {
+    res.render('alunos.ejs', {alunos: await buscaNome(req.body.Nome)});
+});
+
 app.get('/alunos/remover/:id', async (req, res) => {
     Aluno.destroy({where: {id: req.params.id}});
 
@@ -38,37 +43,37 @@ app.get('/alteracao/:id', async (req, res) => {
 });
 
 app.post('/alteracao/:id', async (req, res) => {
-    try {
-        if (req.params.id == 0) {
-            await Aluno.create({
-                matricula: req.body.Matricula,
-                nome: req.body.Nome,
-                dataDeNascimento: req.body.DataDeNascimento,
-                email: req.body.Email,
-                status: req.body.Status
-            });
-        }
-
-        else {
-            await Aluno.update({
-                matricula: req.body.Matricula,
-                nome: req.body.Nome,
-                dataDeNascimento: req.body.DataDeNascimento,
-                email: req.body.Email,
-                status: req.body.Status,
-            },
-
-            {where: {id: req.params.id}});
-        }
+    if (req.params.id == 0) {
+        await Aluno.create({
+            matricula: req.body.Matricula,
+            nome: req.body.Nome,
+            dataDeNascimento: req.body.DataDeNascimento,
+            email: req.body.Email,
+            status: req.body.Status
+        });
     }
 
-    catch (erro) {
-        console.log(erro);
-    }
+    else {
+        await Aluno.update({
+            matricula: req.body.Matricula,
+            nome: req.body.Nome,
+            dataDeNascimento: req.body.DataDeNascimento,
+            email: req.body.Email,
+            status: req.body.Status,
+        },
 
-    finally {
-        res.redirect('/alunos');
+        {where: {id: req.params.id}});
     }
+    
+    res.redirect('/alunos');
 });
+
+async function buscaNome(nome) {
+    const {Op} = Sequelize;
+
+    return await Aluno.findAll({
+        where: {nome: {[Op.like]: `%${nome}%`}}
+    });
+}
 
 export default app;
